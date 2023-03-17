1e0494acf8fb62d012c00d24696cc8a3dc35d8e3
rifqideveloper <zombititi@gmail.com>
2023-03-17 09:20:13 +0700

    auto update

--- bin/index.service.worker.js
+++ bin/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1678519875|4475010977";
+const CACHE_VERSION = "1679019510|4756852178";
 const CACHE_PREFIX = "codeartstudio-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
